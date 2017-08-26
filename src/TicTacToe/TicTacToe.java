e2ce24d9a7db0aa81578b9bfee35aec9acab5956
kyle-walser <kyle.walser@hotmail.com>
2017-08-26 16:57:37 -0400

    added TicTacToe
    added Start of file for tictactoe

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -35,8 +35,8 @@ public class TicTacToe extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
 	}
 
 }
