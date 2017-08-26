98c5fb70846e74827e907731f69539e56e646694
kyle-walser <kyle.walser@hotmail.com>
2017-08-26 18:16:28 -0400

    update logic to buttons

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -10,14 +10,21 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
+
 import javax.swing.JSeparator;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 public class TicTacToe extends JFrame {
 
 	private JPanel contentPane;
+	private String mark;
+	
+	
 
 	/**
 	 * Launch the application.
@@ -39,6 +46,7 @@ public class TicTacToe extends JFrame {
 	 * Create the frame.
 	 */
 	public TicTacToe() {
+		setMarkChoice();
 		setTitle("TIC TAC TOE");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 299, 322);
@@ -57,49 +65,103 @@ public class TicTacToe extends JFrame {
 		contentPane.setLayout(null);
 		
 		JButton btn1 = new JButton("");
+		btn1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btn1.setEnabled(false);
+				btn1.setText(getMarkChoice());
+			}
+		});
 		btn1.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		btn1.setBounds(10, 34, 76, 45);
 		contentPane.add(btn1);
 		
 		JButton btn2 = new JButton("");
+		btn2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btn2.setEnabled(false);
+				btn2.setText(getMarkChoice());
+			}
+		});
 		btn2.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		btn2.setBounds(96, 34, 76, 45);
 		contentPane.add(btn2);
 		
 		JButton btn3 = new JButton("");
+		btn3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btn3.setEnabled(false);
+				btn3.setText(getMarkChoice());
+			}
+		});
 		btn3.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		btn3.setBounds(182, 34, 76, 45);
 		contentPane.add(btn3);
 		
 		JButton btn4 = new JButton("");
+		btn4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btn4.setEnabled(false);
+				btn4.setText(getMarkChoice());
+			}
+		});
 		btn4.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		btn4.setBounds(10, 90, 76, 45);
 		contentPane.add(btn4);
 		
 		JButton btn5 = new JButton("");
+		btn5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btn5.setEnabled(false);
+				btn5.setText(getMarkChoice());
+			}
+		});
 		btn5.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		btn5.setBounds(96, 90, 76, 45);
 		contentPane.add(btn5);
 		
 		JButton btn6 = new JButton("");
+		btn6.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btn6.setEnabled(false);
+				btn6.setText(getMarkChoice());
+			}
+		});
 		btn6.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		btn6.setBounds(182, 90, 76, 45);
 		contentPane.add(btn6);
 		
 		JButton btn7 = new JButton("");
+		btn7.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btn7.setEnabled(false);
+				btn7.setText(getMarkChoice());
+			}
+		});
 		btn7.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		btn7.setBounds(10, 146, 76, 45);
 		contentPane.add(btn7);
 		
 		JButton btn8 = new JButton("");
+		btn8.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btn8.setEnabled(false);
+				btn8.setText(getMarkChoice());
+			}
+		});
 		btn8.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		btn8.setBounds(96, 146, 76, 45);
 		contentPane.add(btn8);
 		
-		JButton button = new JButton("");
-		button.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		button.setBounds(182, 146, 76, 45);
-		contentPane.add(button);
+		JButton btn9 = new JButton("");
+		btn9.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btn9.setEnabled(false);
+				btn9.setText(getMarkChoice());
+			}
+		});
+		btn9.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		btn9.setBounds(182, 146, 76, 45);
+		contentPane.add(btn9);
 		
 		JSeparator separator = new JSeparator();
 		separator.setBounds(0, 202, 283, 2);
@@ -108,10 +170,31 @@ public class TicTacToe extends JFrame {
 		JButton btnExit = new JButton("Exit");
 		btnExit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				dispose();
+				System.exit(0);
 			}
 		});
 		btnExit.setBounds(182, 228, 89, 23);
 		contentPane.add(btnExit);
+	}
+	private void setMarkChoice(){
+		boolean set = false;
+		do{
+		mark = JOptionPane.showInputDialog(null, "Choose your marker: X or O ", "Marker Choice", JOptionPane.INFORMATION_MESSAGE).toUpperCase();
+		System.out.println(mark);
+		
+		if (mark.equals("X") == true){
+			set = true;
+		}else if( mark.equals("O") == true){
+			set = true;
+		}else
+		{
+			JOptionPane.showMessageDialog(null, mark + " This is not a valid marker.\n Please try again", "Marker Choice Error", JOptionPane.ERROR_MESSAGE);
+		}
+		System.out.println(set);
+		
+		} while (set == false);
+	}
+	private String getMarkChoice(){
+		return mark;
 	}
 }
